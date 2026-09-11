dc3808e9ee812b0fba835c1b6c853df6147b68d5
jalovec8 <janjalovec@hotmail.com>
2026-09-11 20:07:07 +0200

    zakladni menu, funkcni start a exit (4.5h btw)

--- game-jam-26/Code/Menus/Startingmenu.cs
+++ game-jam-26/Code/Menus/Startingmenu.cs
@@ -1,15 +1,38 @@
 using Godot;
 using System;
+using System.Net.Quic;
 
 public partial class Startingmenu : Control
 {
 	// Called when the node enters the scene tree for the first time.
+	
+	private TextureButton _startButton;
+	private TextureButton _exitButton;
 	public override void _Ready()
 	{
+
+	
+		var zoom = 5f;
+		var lookAt = new Vector2(15, 15);
+		var half = GetViewportRect().Size / 2f;
+
+		Scale = new Vector2(zoom, zoom);
+		Position = half - lookAt * zoom;
+
+	}
+	public void OnClickButtonStart()
+	{
+	Hide();
+	}
+
+	public void OnClickButtonExit()
+	{
+		GetTree().Quit(0); 
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{
-	}
+	{ 
+		
+	}	
 }
