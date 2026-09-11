4f2f1e9b2b27e5bccc3aa1070bf8dfe494885d29
ViktorsakYT <viktorsvancar174@gmail.com>
2026-09-11 18:25:29 +0200

    Slopified controls (feels nicer imo)

--- game-jam-26/Code/Player/Player.cs
+++ game-jam-26/Code/Player/Player.cs
@@ -4,6 +4,9 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
+	public const float DecelFactor = 5.0f; 
+	public const float AccelFactor = 1.0f; 
+
 	public const float JumpVelocity = -400.0f;
 
 	public override void _PhysicsProcess(double delta)
@@ -25,14 +28,16 @@ public partial class Player : CharacterBody2D
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("left", "right", "up", "down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+		// if (direction != Vector2.Zero)
+		// {
+			// velocity.X = direction.X * Speed;
+			// velocity.Y = direction.Y * Speed;
+
+
+
+			velocity.X = Mathf.MoveToward(velocity.X, direction.X * Speed, Speed / DecelFactor);
+			velocity.Y = Mathf.MoveToward(velocity.Y, direction.Y * Speed, Speed / DecelFactor);
+
 
 		Velocity = velocity;
 		MoveAndSlide();
