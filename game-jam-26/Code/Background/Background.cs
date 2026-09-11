d7669db4413ad4b88adce4433b2ff4a021870a14
ViktorsakYT <viktorsvancar174@gmail.com>
2026-09-11 18:49:41 +0200

    Background waviness shenanigans

--- game-jam-26/Code/Background/Background.cs
+++ game-jam-26/Code/Background/Background.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Reflection.Metadata;
+using System.Transactions;
 
 public partial class Background : Node2D
 {
@@ -9,7 +11,17 @@ public partial class Background : Node2D
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
+	public const float WobbleSpeed = 0.3f;
+	public const float WobbleRange = 0.01f;
+	public const float RisingSpeed = 0.05f;
+	float phase = 0.0f;
 	public override void _Process(double delta)
 	{
+		phase += WobbleSpeed;
+		phase %= 360;
+
+		
+		Position += new Vector2((float)(Math.Sin((phase / 360) * 0.9f * Math.PI * 2) * WobbleRange / 2 + Math.Sin((phase / 360) * 1.1f *  Math.PI * 2) * WobbleRange / 2), 0);
 	}
 }
