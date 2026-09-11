4216ef22dfb220ad71a5901198c19293ff9bc47f
ViktorsakYT <viktorsvancar174@gmail.com>
2026-09-11 19:23:02 +0200

    Scrolling background babyyyy

---
+++ game-jam-26/Code/Background/ScrollingBGsprite.cs
@@ -0,0 +1,34 @@
+using Godot;
+
+public partial class ScrollingBGsprite : Sprite2D
+{
+    [Export] public float ScrollSpeed { get; set; } = 10.0f;
+    [Export] public float WindowHeight { get; set; } = 500.0f;
+
+    private float _offsetY;
+    private float _textureHeight;
+
+    public override void _Ready()
+    {
+		GD.Print("Ready called!");
+        RegionEnabled = true;
+        TextureRepeat = CanvasItem.TextureRepeatEnum.Enabled;
+        _textureHeight = Texture.GetSize().Y;
+        UpdateRegion();
+    }
+
+    public override void _Process(double delta)
+    {
+
+					GD.Print("hello world");
+
+        _offsetY += ScrollSpeed * -(float)delta;
+        _offsetY %= _textureHeight;
+        UpdateRegion();
+    }
+
+    private void UpdateRegion()
+    {
+        RegionRect = new Rect2(0, _offsetY, Texture.GetSize().X, WindowHeight);
+    }
+}
