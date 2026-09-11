ed85d0d72543ebc45f963f0cc1ba9af35f1ba3d0
ViktorsakYT <viktorsvancar174@gmail.com>
2026-09-11 20:06:30 +0200

    More food spawns

--- game-jam-26/Code/Core/Main.cs
+++ game-jam-26/Code/Core/Main.cs
@@ -4,8 +4,19 @@ using System;
 public partial class Main : Node
 {
 	// Called when the node enters the scene tree for the first time.
+	private RandomNumberGenerator _rng = new RandomNumberGenerator();
+
 	public override void _Ready()
 	{
+
+		PackedScene foodScene = GD.Load<PackedScene>("res://Scenes/Misc/fishfood.tscn");
+
+
+		for(int i = 0; i < 500; i++) {
+		Fishfood food = foodScene.Instantiate<Fishfood>();
+        food.Position = new Vector2(100 * _rng.RandfRange(0,1), 100* _rng.RandfRange(0,1));
+        AddChild(food);
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
