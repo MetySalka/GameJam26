78950c794c7c6969b547de100cc07b171a0470fc
jalovec8 <janjalovec@hotmail.com>
2026-09-12 09:02:34 +0200

    bar reguje pouze na jidlo

--- game-jam-26/Code/Misc/Fishfood.cs
+++ game-jam-26/Code/Misc/Fishfood.cs
@@ -10,17 +10,22 @@ public partial class Fishfood : Area2D
 
 	// Local movement applied by Main each frame.
 	public Vector2 MoveVector { get; set; }
-	
-	
+
+
 	public void Consume(bool Do = true)
 	{
 		// QueueFree runs later, so ignore duplicate pickups in the same frame.
-		
+
 		if (_consumed)
 			return;
-
 		_consumed = true;
+		
+		if (Do)
+		{
+			GetNode<TextureProgressBar>("/root/Main/ProgressBar/TextureProgressBar").Value += 5;
+		}
+		
 		QueueFree();
-		GetNode<TextureProgressBar>("/root/Main/ProgressBar/TextureProgressBar").Value += 5;
+		
 	}
 }
