2f17f94322b99757aa71b8bbde4bf756e82825b0
jalovec8 <janjalovec@hotmail.com>
2026-09-12 08:48:31 +0200

    funkcni progressbar 0-100

--- game-jam-26/Code/UI/ProgressBar.cs
+++ game-jam-26/Code/UI/ProgressBar.cs
@@ -8,13 +8,12 @@ public partial class ProgressBar : Node2D
 	[Export] private TextureProgressBar _textureBar;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{
-		var barValue = _textureBar;
-		GD.Print(barValue);
+	{ 
+		_textureBar.Value = 0;
 	}
-
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GD.Print(_textureBar.Value);
 	}
 }
