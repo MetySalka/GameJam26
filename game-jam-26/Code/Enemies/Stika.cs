ecebffd847653b5e050b8a56b8c7e973ef35f6b7
ViktorsakYT <viktorsvancar174@gmail.com>
2026-09-12 13:58:35 +0200

    tinyPatch

--- game-jam-26/Code/Enemies/Stika.cs
+++ game-jam-26/Code/Enemies/Stika.cs
@@ -112,8 +112,9 @@ public partial class Stika : Area2D
 
 	public void Launch(Vector2 Target)
 	{
+		int v = _rng.RandiRange(250, 700);
 		float angle = Helpers.GetAngleToObject(GlobalPosition, Target);
-		Velocity = new Vector2(Mathf.Cos(Mathf.DegToRad(angle)) * 350, Mathf.Sin(Mathf.DegToRad(angle)) * 350);
+		Velocity = new Vector2(Mathf.Cos(Mathf.DegToRad(angle)) * v, Mathf.Sin(Mathf.DegToRad(angle)) * v);
 		countDown = -960;
 		Seek = false;
 	}
