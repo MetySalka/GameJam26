50413aba7a4d690e4562d8014cb62a91a932a64c
MetySalka <mety.holan@outlook.cz>
2026-09-12 20:59:13 +0200

    Mouth remake

--- game-jam-26/Code/Player/Player.cs
+++ game-jam-26/Code/Player/Player.cs
@@ -140,7 +140,6 @@ public partial class Player : CharacterBody2D
 			"Right" => 2,
 			_ => 3
 		};
-		_pickupArea.SetMouthDir(direction);
 		_hitbox.SetHitDir(direction);
 	}
 
@@ -191,27 +190,23 @@ public partial class Player : CharacterBody2D
 		if (velocity.Y >= 0.2)
 		{
 			_sprite.Play("Down");
-			_pickupArea.SetMouthDir(0);
 			_hitbox.SetHitDir(0);
 		}
 		else if (velocity.Y <= -0.2)
 		{
 			_sprite.Play("Up");
-			_pickupArea.SetMouthDir(1);
 			_hitbox.SetHitDir(1);
 		}
 
 		if (velocity.X >= 0.2)
 		{
 			_sprite.Play("Right");
-			_pickupArea.SetMouthDir(2);
 			_hitbox.SetHitDir(2);
 		}
 		else if (velocity.X <= -0.2)
 		{
 
 			_sprite.Play("Left");
-			_pickupArea.SetMouthDir(3);
 			_hitbox.SetHitDir(3);
 		}
 
